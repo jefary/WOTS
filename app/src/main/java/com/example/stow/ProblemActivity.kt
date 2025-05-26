f8d363c01f7a3faeaa9575cedb134f48d5499c2a
jefary <dev.vegabrian@gmail.com>
2025-05-25 22:13:34 -0700

    Initial commit

--- app/src/main/java/com/example/stow/ProblemActivity.kt
+++ app/src/main/java/com/example/stow/ProblemActivity.kt
@@ -5,24 +5,16 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import android.content.Intent
-import android.widget.Button
 
-class MainActivity : AppCompatActivity() {
+class ProblemActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_problem)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-        val threeBarButton = findViewById<Button>(R.id.threeBar)
-        threeBarButton.setOnClickListener {
-            val intent = Intent(this, ThreeBarActivity::class.java)
-            startActivity(intent)
         }
     }
 }
--- app/src/main/java/com/example/stow/ThreeBarActivity.kt
+++ app/src/main/java/com/example/stow/ThreeBarActivity.kt
@@ -5,23 +5,23 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import android.content.Intent
 import android.widget.Button
+import android.content.Intent
 
-class MainActivity : AppCompatActivity() {
+class ThreeBarActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_threebar)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        val threeBarButton = findViewById<Button>(R.id.threeBar)
-        threeBarButton.setOnClickListener {
-            val intent = Intent(this, ThreeBarActivity::class.java)
+        val exitButton = findViewById<Button>(R.id.exit)
+        exitButton.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
     }
