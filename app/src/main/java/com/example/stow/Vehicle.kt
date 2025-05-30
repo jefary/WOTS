d2704279658acecc55fa0ede24a802cf44a449ee
jefary <dev.vegabrian@gmail.com>
2025-05-29 21:58:44 -0700

    Just created a Vehicle that can add and remove. I need to create Bins with items in and be able to remove it

--- app/src/main/java/com/example/stow/Vehicle.kt
+++ app/src/main/java/com/example/stow/Vehicle.kt
@@ -1,5 +1,26 @@
 package com.example.stow
 
 class Vehicle {
-    val order: Order
+    private var _id: String,
+    val hashMap = HashMap<String, Items>()
+
+    var id: String
+        get() = _id
+        set(value) {
+            if (value.isNotEmpty()) {
+                _id = value
+            }
+        }
+
+    fun addItem(i: Items) {
+        hashMap.put(i.prodName, i)
+    }
+
+    fun getItem(i: Items) {
+        hashMap.get(i.prodName)
+    }
+
+    fun removeItem(i: Items) {
+        hashMap.remove(i.prodName)
+    }
 }
